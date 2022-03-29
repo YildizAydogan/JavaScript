5dd681a3414485180413f9379a36ac8caafc4ba4
YildizAydogan <yildizaydoganjobmail@gmail.com>
2022-03-29 19:21:25 +0200

    JS

--- assets/js/17-hg.js
+++ assets/js/17-hg.js
@@ -1,11 +1,11 @@
 function darkMode() {
-    var element = document.table;
+    var element = document.body;
     element.classList.toggle("dark-mode");
     document.getElementById("switch").innerHTML = "Toggle Light Mode";
   }
   
   function lightMode() {
-    var element = document.table;
+    var element = document.body;
     element.classList.toggle("dark-mode");
     document.getElementById("switch").innerHTML = "Toggle Dark Mode";
   }
